d0a88c77b8024de6632773ab3fec3e3f39eed460
István Ignácz <ignaczistvan@gmail.com>
2017-02-20 01:17:20 +0100

    Downgrade 'hello' command to the reality

--- src/controllers/hello.js
+++ src/controllers/hello.js
@@ -15,7 +15,6 @@ function* generator(bot, req) {
     user: req.user,
     body: `*Things I can help you with:*\n
   - "caniuse _feature_" for complete coverage\n
-  - "caniuse _feature_ in _browser_ _version_" for browser specific support\n
 You may leave the 'caniuse' part if you send me a private message. You can also mention me with an @`,
     params: {
       as_user: true,
